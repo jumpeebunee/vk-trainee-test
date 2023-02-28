4d4c32e19ae9cc9a7e4055216de8c7991fe38ba2
Ben <jumpeebunee@gmail.com>
2023-02-28 10:39:19 +0300

    fix: fix mines

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import React from 'react'
 const App = () => {
 
   const TOTAL__FIELDS = 16;
+  const TOTAL__MINES = 40;
 
-  const getMines = (totalMines: number) => {
+  const getMines = () => {
     const mines:any = [];
 
-    while (mines.length < totalMines) {
+    while (mines.length < TOTAL__MINES) {
       const newMine = {x: getRandomNumber(TOTAL__FIELDS), y: getRandomNumber(TOTAL__FIELDS)};
       const isAdded = mines.find((item:any) => {
         return item.x === newMine.x && item.y === newMine.y;
@@ -18,11 +19,9 @@ const App = () => {
     return mines;
   }
 
-  const getRandomNumber = (max: number) => {
-    return Math.floor(Math.random() * (max - 0) + 0);
-  }
-
   const getFields = () => {
+    const mines = getMines();
+    console.log(mines)
     const board = [];
     for (let x = 0; x < TOTAL__FIELDS; x += 1) {
       const row = [];
@@ -31,6 +30,7 @@ const App = () => {
           elem: <li className='app__field'></li>,
           x,
           y,
+          isMine: mines.find((mine: any) => mine.x === x && mine.y === y),
         }
         row.push(cell);
       }
@@ -39,7 +39,9 @@ const App = () => {
     return board;
   }
 
-  getMines(10);
+  const getRandomNumber = (max: number) => {
+    return Math.floor(Math.random() * (max - 0) + 0);
+  }
 
   return (
     <div className='app'>
@@ -47,7 +49,7 @@ const App = () => {
         <ul className='app__fields'>
           {getFields().map(board =>
             board.map(field => 
-              <>{field.elem}</>
+              <div className={field.isMine ? 'mine' : ''}>{field.elem}</div>
             ))}
         </ul>
       </div>
